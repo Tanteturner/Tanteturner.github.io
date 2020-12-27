56e794c7d8573f410eb1ec4bbecccc6354023437
GameDevDummy <gamedevdummy.contact@gmail.com>
2020-12-27 15:00:53 +0100

    com it

--- polyfills.js
+++ polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Files\Workspace\PhpstormProjects\Tanteturner\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! D:\Files\Workspace\PhpstormProjects\TanteturnerProjects\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
